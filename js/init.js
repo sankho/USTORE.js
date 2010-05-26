4b9a9b7ccb439e57e1e6be112225a717b3b735f1
Sankho Mallik <smallik@hugeinc.com>
2010-05-26 14:22:15 -0400

    Initial commit of files to Git repo.

---
+++ js/init.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    
+    $('input[type="text"]').val('');
+    
+    var key     = $('#key'),
+        value   = $('#value'),
+        search  = $('#search-key'),
+        ans     = $('#ans'),
+        del     = $('#delete');
+        
+    
+    $('#save').click(function() {
+        if (key.val() !== '' && value.val() !== '') {
+            var session = $('#session').is(':checked');
+            $.userStorage.setValue(key.val(), value.val(), session);
+        } else {
+            alert('Please enter both a key and a value');
+        }
+    });
+    
+    $('#search').click(function() {
+        if (search.val() !== '') {
+            var session = $('#search-session').is(':checked');
+            var result = $.userStorage.getValue(search.val(), session);
+            ans.html(search.val() + ' = ' + result);
+        } else {
+            alert('Please enter key to search for');
+        }
+    });
+    
+    $('#delete-btn').click(function() {
+        if (del.val() !== '') {
+            var session = $('#delete-session').is(':checked');
+            $.userStorage.deleteValue(del.val(), session);
+        } else {
+            alert('Please enter a key');
+        }
+    });
+    
+});
